b2c34f58cc5385ff22b5d958ef76394e9fb6ba51
aqveduk <kingdon@ya.ru>
2018-11-27 01:02:38 +0300

    Task 2 added

--- Program.cs
+++ Program.cs
@@ -1,15 +1,44 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 
-namespace Task1
+
+namespace ConsoleApp2
 {
+    public delegate double Fun(double x, double a);
+
     class Program
     {
+        public static void Table(Fun F, double x, double b, double a)
+        {
+
+            Console.WriteLine(" ----- X ----- Y ----- ");
+            while (x <= b)
+            {
+                Console.WriteLine("| {0,8:0.000} | {1,8:0.000} |", x, F(x,a));
+                x += 1;
+            }
+            Console.WriteLine("---------------------");
+
+        }
+        public static double MyFunc(double x, double a)
+        {
+            return a * (x * x);
+        }
+        public static double SinFun(double x, double a)
+        {
+            return a * Math.Sin(x);
+        }
+
         static void Main(string[] args)
         {
+            int a = 0;
+            Console.WriteLine(" Table of function MyFunc (a * x ^ 2): ");
+            Table(MyFunc, -6, 6, 5);
+
+            Console.WriteLine(" Table of funcion a * Sin(x) (x = rad): ");
+            Table(SinFun, -6, 6, 5);
+
+
+            Console.ReadKey();
         }
     }
 }
---
+++ Task2/Program.cs
@@ -0,0 +1,102 @@
+﻿using System;
+using System.IO;
+
+namespace DoubleBinary
+{
+    class Program
+    {
+        /// <summary>
+        /// delegate functions with 1 parametr x
+        /// </summary>
+        /// <param name="x"></param>
+        /// <returns></returns>
+        public delegate double Fun(double x);
+
+        public static double F(double x)
+        {
+            return x * x - 50 * x + 10;
+        }
+        /// <summary>
+        /// make the power times 3
+        /// </summary>
+        /// <param name="x"></param>
+        /// <returns></returns>
+        public static double ThirdPow(double x)
+        {
+            return x * x * x;
+        }
+
+        public static void Menu()
+        {
+            bool flag = true;
+            Console.WriteLine("Welcome! Please select a function: \n1. X^2 - 50*x + 10\n2.x^3");
+            while (flag)
+            {
+                string answer = Console.ReadLine();
+
+
+                Console.WriteLine("Please enter the first value for range beginning: ");
+                string min = Console.ReadLine();
+
+                Console.WriteLine("Please enter the second value for range end: ");
+                string max = Console.ReadLine();
+
+
+                if (answer == "1")
+                {
+
+                    Console.Write("Result: ");
+                    SaveFunc("data.bin", F, double.Parse(min), double.Parse(max), 1);
+                    Console.WriteLine(Load("data.bin"));
+                    flag = false;
+                }
+                else if (answer == "2")
+                {
+                    Console.Write("Result: ");
+                    SaveFunc("data.bin", ThirdPow, double.Parse(min), double.Parse(max), 1);  //power times 3
+                    Console.WriteLine(Load("data.bin"));
+                    flag = false;
+                }
+                else
+                    Console.WriteLine("Wrong value! Try again!");
+            }
+        }
+
+        public static void SaveFunc(string fileName, Fun Fu, double a, double b, double h)
+        {
+            FileStream fs = new FileStream(fileName, FileMode.Create, FileAccess.Write);
+            BinaryWriter bw = new BinaryWriter(fs);
+            double x = a;
+            while (x <= b)
+            {
+                bw.Write(Fu(x));
+                x += h;
+            }
+            bw.Close();
+            fs.Close();
+        }
+        public static double Load(string fileName)
+        {
+            FileStream fs = new FileStream(fileName, FileMode.Open, FileAccess.Read);
+            BinaryReader bw = new BinaryReader(fs);
+            double min = double.MaxValue;
+            double d;
+            for (int i = 0; i < fs.Length / sizeof(double); i++)
+            {
+                d = bw.ReadDouble();
+                if (d < min) min = d;
+            }
+            bw.Close();
+            fs.Close();
+            return min;
+        }
+        static void Main(string[] args)
+        {
+            Menu();
+
+            Console.ReadKey();
+        }
+    }
+
+
+}
